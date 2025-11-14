c64016a9704d990b2c9c4cb07b112bb3586ab793
flowwebdevDE <info@flowwebdesign.de>
2025-11-14 09:33:47 +0100

    Add files via upload

--- app/data/iphone/ios.js
+++ app/data/iphone/ios.js
@@ -1,8 +1,12 @@
 const ua = navigator.userAgent.toLowerCase();
     const isIOS = /iphone|ipad|ipod/.test(ua);
     const isStandalone = window.navigator.standalone === true;
+    // const isAndroid
 
     // Nur iOS + WebApp → eigenes Styling aktivieren
     if (isIOS && isStandalone) {
       document.body.classList.add("ios-webapp");
+    }
+    if (isIOS) {
+        dovument.body.classList.add("only-android")
     }
